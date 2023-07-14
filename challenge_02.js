9eedc4dc582f5a1f5b59011eeac2b470ab5be9fd
nobodyfreeze <office.fherzog@gmail.com>
2023-07-14 12:56:27 +0200

    solution 1

--- challenge_02.js
+++ challenge_02.js
@@ -12,8 +12,31 @@ Alter 6-12 // Saft
 Alter 13-17 // Cola
 18 + // Wein
 
--->zB: "Max trinkt Cola."
+--> zB: "Max trinkt Cola."
 
 /* Getränke-Challenge */
 
 
+// Eingabe des Namens über eine Prompt-Anforderung
+var name = prompt("Geben Sie Ihren Namen ein:");
+
+// Eingabe des Alters über eine Prompt-Anforderung
+var age = parseInt(prompt("Geben Sie Ihr Alter ein:"));
+
+// Ausgabe der Getränkeauswahl abhängig vom Alter
+var drink;
+
+if (age >= 0 && age <= 5) {
+  drink = "Milch";
+} else if (age >= 6 && age <= 12) {
+  drink = "Saft";
+} else if (age >= 13 && age <= 17) {
+  drink = "Cola";
+} else if (age >= 18) {
+  drink = "Wein";
+} else {
+  drink = "unbekannt";
+}
+
+// Ausgabe des Satzes mit dem Namen und dem entsprechenden Getränk
+console.log(name + " trinkt " + drink + ".");
