689439bd46a675711077addcf83c0a10321e2bf9
nobodyfreeze <office.fherzog@gmail.com>
2023-07-17 09:10:17 +0200

    var firstName

--- challenge_02.js
+++ challenge_02.js
@@ -18,56 +18,56 @@ Alter 13-17 // Cola
 
 /************** Lösung 1 **************/
 
-// Schritt 1: Deklaration der Variablen
-var name, age, drink;
-
-// Schritt 2: Eingabe des Namens über eine Prompt-Anforderung
-name = prompt("Geben Sie Ihren Namen ein:");
-
-// Schritt 3: Eingabe des Alters über eine Prompt-Anforderung und Konvertierung in eine Zahl
-age = parseInt(prompt("Geben Sie Ihr Alter ein:"));
-
-// Schritt 4: Ausgabe der Getränkeauswahl abhängig vom Alter
-if (age >= 0 && age <= 5) {
-  drink = "Milch";
-} else if (age >= 6 && age <= 12) {
-  drink = "Saft";
-} else if (age >= 13 && age <= 17) {
-  drink = "Cola";
-} else if (age >= 18) {
-  drink = "Wein";
-} else {
-  drink = "unbekannt";
-}
-
-// Ausgabe des Satzes mit dem Namen und dem entsprechenden Getränk
-console.log(name + " trinkt " + drink + ".");
-
-/************** Lösung 2 **************/
-
 // // Schritt 1: Deklaration der Variablen
-// var name, age, drink;
+// var firstName, age, drink;
 
 // // Schritt 2: Eingabe des Namens über eine Prompt-Anforderung
-// name = prompt("Geben Sie Ihren Namen ein:");
+// firstName = prompt("Geben Sie Ihren Namen ein:");
 
 // // Schritt 3: Eingabe des Alters über eine Prompt-Anforderung und Konvertierung in eine Zahl
 // age = parseInt(prompt("Geben Sie Ihr Alter ein:"));
 
-// switch (true) {
-//   case (age >= 0 && age <= 5):
-//     drink = "Milch";
-//     break;
-//   case (age >= 6 && age <= 12):
-//     drink = "Saft";
-//     break;
-//   case (age >= 13 && age <= 17):
-//     drink = "Cola";
-//     break;
-//   default:
-//     drink = "Wein";
-//     break;
+// // Schritt 4: Ausgabe der Getränkeauswahl abhängig vom Alter
+// if (age >= 0 && age <= 5) {
+//   drink = "Milch";
+// } else if (age >= 6 && age <= 12) {
+//   drink = "Saft";
+// } else if (age >= 13 && age <= 17) {
+//   drink = "Cola";
+// } else if (age >= 18) {
+//   drink = "Wein";
+// } else {
+//   drink = "unbekannt";
 // }
 
 // // Ausgabe des Satzes mit dem Namen und dem entsprechenden Getränk
-// console.log(name + " trinkt " + drink + ".");
+// console.log(firstName + " trinkt " + drink + ".");
+
+/************** Lösung 2 **************/
+
+// Schritt 1: Deklaration der Variablen
+var firstName, age, drink;
+
+// Schritt 2: Eingabe des Namens über eine Prompt-Anforderung
+firstName = prompt("Geben Sie Ihren Namen ein:");
+
+// Schritt 3: Eingabe des Alters über eine Prompt-Anforderung und Konvertierung in eine Zahl
+age = parseInt(prompt("Geben Sie Ihr Alter ein:"));
+
+switch (true) {
+  case (age >= 0 && age <= 5):
+    drink = "Milch";
+    break;
+  case (age >= 6 && age <= 12):
+    drink = "Saft";
+    break;
+  case (age >= 13 && age <= 17):
+    drink = "Cola";
+    break;
+  default:
+    drink = "Wein";
+    break;
+}
+
+// Ausgabe des Satzes mit dem Namen und dem entsprechenden Getränk
+console.log(firstName + " trinkt " + drink + ".");
